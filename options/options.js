a8dbcc8bdb168000ff39b17d9a3b40f7670af00d
gloomy-ghost <gloomy-ghost@users.noreply.github.com>
2017-02-18 05:39:57 +0000

    Import blacklist, v0.1.5

--- options/options.js
+++ options/options.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-	let addButton = document.getElementById('addButton'),
+	let importButton = document.getElementById('importButton'),
+	addButton = document.getElementById('addButton'),
 	removeButton = document.getElementById('removeButton'),
+	importURL = document.getElementById('importURL'),
 	newWhitelistDomain = document.getElementById('newWhitelistDomain'),
 	excludedDomainsBox = document.getElementById('excludedDomainsBox');
   
@@ -11,20 +13,36 @@ document.addEventListener('DOMContentLoaded', function() {
 		document.getElementById(boxId).appendChild(elt);
 	}
 	function removeFromListBox(boxId, text) {
-		var list = document.getElementById(boxId);
-		for (var i = 0; i < list.length; i++)
+		let list = document.getElementById(boxId);
+		for (let i = 0; i < list.length; i++)
 			if (list.options[i].value == text)
 				list.remove(i--);
 	}
 
 	// existing state
-	for(var i = 0 ; i < localStorage.length; i++){
+	for(let i = 0 ; i < localStorage.length; i++){
 		domain = localStorage.key(i);
 		if (Number(localStorage.getItem(domain)) === 1)
 			appendToListBox('excludedDomainsBox', domain);
 	}
   
 	// click listener
+	importButton.addEventListener('click', function () {
+		let request = new XMLHttpRequest();
+		request.open('GET', importURL.value, true);
+		request.onload = function() {
+		  if (this.status >= 200 && this.status < 400) {
+			let data = JSON.parse(this.response);
+			for (let domain in data)
+				localStorage[domain] = data[domain];
+			location.reload();
+		  } else {
+			alert('Something Happened.');
+		  }
+		};
+		request.send();
+	});
+
 	addButton.addEventListener('click', function () {
 		localStorage[newWhitelistDomain.value] = 1;
 		appendToListBox('excludedDomainsBox', newWhitelistDomain.value);
@@ -33,13 +51,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	removeButton.addEventListener('click', function () {
 		let remove = [];
-		for (var i = 0; i < excludedDomainsBox.length; i++)
+		for (let i = 0; i < excludedDomainsBox.length; i++)
 			if (excludedDomainsBox.options[i].selected)
 				remove.push(excludedDomainsBox.options[i].value);
 		if (!remove.length)
 			return;
 
-		for (var i = 0; i < remove.length; i++){
+		for (let i = 0; i < remove.length; i++){
 			localStorage[remove[i]] = 0;
 			removeFromListBox('excludedDomainsBox', remove[i]);
 		}
